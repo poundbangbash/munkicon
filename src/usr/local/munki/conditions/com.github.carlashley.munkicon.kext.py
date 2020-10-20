c880ee05eb0f2b34c25d29f0667e2b9962db451e
carlashley <carl@macapakr.com>
2020-10-20 17:44:21 +1000

    Refactor KEXT

--- src/usr/local/munki/conditions/com.github.carlashley.munkicon.kext.py
+++ src/usr/local/munki/conditions/com.github.carlashley.munkicon.kext.py
@@ -1,105 +1,114 @@
 #!/usr/local/munki/python
 import os
 import sqlite3
-import sys
 
 try:
     from munkicon import worker
 except ImportError:
     from .munkicon import worker
 
+# Keys: 'kext_teams'
+#       'kext_bundles'
+#       'kext_team_bundle'
 
-class SQLite():
-    """Simple class for connecting/disconnecting to SQLite databases."""
-    connection = ''
-    c = ''
 
-    def connect(self, db):
+class SQLiteDB():
+    """SQLite"""
+    def __init__(self, db='/var/db/SystemPolicyConfiguration/KextPolicy'):
+        self._db = db
+        self._connection = None
+
+        self.cursor = None
+
+    def _connect(self):
         """Connect."""
-        try:
-            self.connection.execute("")
-        except Exception:
-            try:
-                self.connection = sqlite3.connect(db)
-                self.c = self.connection.cursor()
-            except Exception:
-                sys.exit(1)
+        self._connection = sqlite3.connect(self._db)
 
-    def disconnect(self, db):
+        self.cursor = self._connection.cursor()
+
+    def _disconnect(self):
         """Disconnect."""
-        try:
-            self.connection.execute("")
-            try:
-                self.connection.close()
-                try:
-                    self.connection.execute("")
-                except Exception:
-                    sys.exit(1)
-            except Exception:
-                sys.exit(1)
-        except Exception:
-            pass
+        self._connection.close()
 
-    def query(self, query_string, fetch=False):
-        """Query."""
-        try:
-            self.c.execute(query_string)
-            if not fetch:
-                self.c.execute(query_string)
-            else:
-                self.c.execute(query_string)
-                return self.c.fetchall()
-        except Exception:
-            raise
+    def query(self, q):
+        """Query. Fetch all."""
+        result = None
+
+        if os.path.exists(self._db):
+            self._connect()
+
+            try:
+                self.cursor.execute(q)
+
+                _columns = [_desc[0] for _desc in self.cursor.description]
+                _rows = list()
+
+                for _r in self.cursor.fetchall():
+                    _row = {_key: _value for _key, _value in zip(_columns, _r)}
+
+                    _rows.append(self.Row(**_row))
+
+                result = _rows
+            except Exception:
+                self._disconnect()
+                raise
+        return result
+
+    class Row(object):
+        def __init__(self, **kwargs):
+            for _k, _v in kwargs.items():
+                setattr(self, _k, _v)
 
 
 class KextPolicyConditions(object):
     """Whitelisted KEXT's as applied by MDM or set by user."""
     def __init__(self):
-        self._kextpolicy = '/var/db/SystemPolicyConfiguration/KextPolicy'
+        self._mdm_query = 'SELECT team_id, bundle_id, allowed FROM kext_policy_mdm'
+        self._usr_query = 'SELECT team_id, bundle_id, allowed FROM kext_policy'
+        self._db = SQLiteDB()
 
         self.conditions = self._process()
 
+    def _teams(self):
+        """Team ID's"""
+        result = {'kext_teams': list()}
+
+        _mdm = {_x.team_id for _x in self._db.query(q=self._mdm_query)}
+        _usr = {_x.team_id for _x in self._db.query(q=self._usr_query)}
+
+        result['kext_teams'] = list(_mdm.union(_usr))
+
+        return result
+
+    def _bundles(self):
+        """Bundle ID's"""
+        result = {'kext_bundles': list()}
+
+        _mdm = {_x.bundle_id for _x in self._db.query(q=self._mdm_query)}
+        _usr = {_x.bundle_id for _x in self._db.query(q=self._usr_query)}
+
+        result['kext_bundles'] = list(_mdm.union(_usr))
+
+        return result
+
+    def _team_bundles(self):
+        """Team & Bundle ID's"""
+        result = {'kext_bundles': list()}
+
+        _mdm = {'{},{}'.format(_x.team_id, _x.bundle_id) for _x in self._db.query(q=self._mdm_query)}
+        _usr = {'{},{}'.format(_x.team_id, _x.bundle_id) for _x in self._db.query(q=self._usr_query)}
+
+        result['kext_team_bundles'] = list(_mdm.union(_usr))
+
+        return result
+
     def _process(self):
         """Process all conditions and generate the condition dictionary."""
-        result = {'kext_teams': set(),
-                  'kext_bundles': set(),
-                  'kext_team_bundle': set()}
+        result = dict()
 
-        if os.path.exists(self._kextpolicy) and os.geteuid() == 0:  # Must be root to read this file
-            _sqldb = SQLite()
-
-            _sqldb.connect(self._kextpolicy)
-            _mdm_query = _sqldb.query('SELECT team_id, bundle_id, allowed FROM kext_policy_mdm', fetch=True)
-            _user_query = _sqldb.query('SELECT team_id, bundle_id, allowed FROM kext_policy', fetch=True)
-
-            if _mdm_query:
-                for _team_id, _bundle_id, allowed in _mdm_query:
-                    if allowed == 1:
-                        if _team_id:
-                            result['kext_teams'].add(_team_id)
-
-                        if _bundle_id:
-                            result['kext_bundles'].add(_bundle_id)
-
-                        if _team_id and _bundle_id:
-                            result['kext_team_bundle'].add('{},{}'.format(_team_id, _bundle_id))
-
-            if _user_query:
-                for _team_id, _bundle_id, allowed in _user_query:
-                    if allowed == 1:
-                        if _team_id:
-                            result['kext_teams'].add(_team_id)
-
-                        if _bundle_id:
-                            result['kext_bundles'].add(_bundle_id)
-
-                        if _team_id and _bundle_id:
-                            result['kext_team_bundle'].add('{},{}'.format(_team_id, _bundle_id))
-
-        result['kext_teams'] = list(result['kext_teams'])
-        result['kext_bundles'] = list(result['kext_bundles'])
-        result['kext_team_bundle'] = list(result['kext_team_bundle'])
+        result.update(self._teams())
+        result.update(self._bundles())
+        result.update(self._team_bundles())
 
         return result
 
