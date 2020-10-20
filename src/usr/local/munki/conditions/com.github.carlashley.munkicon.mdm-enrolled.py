bfcccbb988ec47435ab6ee930dc91c165bcf3f32
carlashley <carl@macapakr.com>
2020-10-20 18:58:31 +1000

    Refactor MDM

--- src/usr/local/munki/conditions/com.github.carlashley.munkicon.mdm-enrolled.py
+++ src/usr/local/munki/conditions/com.github.carlashley.munkicon.mdm-enrolled.py
@@ -1,7 +1,6 @@
 #!/usr/local/munki/python
 
 import subprocess
-import sys
 
 
 try:
@@ -9,42 +8,43 @@ try:
 except ImportError:
     from .munkicon import worker
 
+# Keys: 'enrolled_via_dep'
+#       'mdm_enrollment'
+
 
 class MDMEnrolledConditions(object):
     def __init__(self):
         self.conditions = self._process()
 
-    def _get_enrollment_state(self):
-        """Check the enrollment state of the device."""
-        result = dict()
-
-        _cmd = ['/usr/bin/profiles', 'status', '-type', 'enrollment']
-
-        _subprocess = subprocess.Popen(_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        _result, _error = _subprocess.communicate()
-
-        if _subprocess.returncode == 0:
-            if _result:
-                _result = _result.decode('utf-8').strip().splitlines()
-
-                for _item in _result:
-                    _key = _item.lower().split(': ')[0].replace(' ', '_')
-                    _value = _item.lower().split(': ')[1].replace(' ', '_').replace('(', '').replace(')', '')
-
-                    result[_key] = _value
-        else:
-            sys.exit(1)
-
-    def _process(self):
-        """Process all conditions and generate the condition dictionary."""
+    def _enrolled_state(self):
+        """MDM/DEP Enrolled State."""
         result = {'enrolled_via_dep': '',
                   'mdm_enrollment': ''}
 
-        _result = self._get_enrollment_state()
+        _cmd = ['/usr/bin/profiles', 'status', '-type', 'enrollment']
+        _p = subprocess.Popen(_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        _r, _e = _p.communicate()
 
-        if _result:
-            result['enrolled_via_dep'] = result.get('enrolled_via_dep', '')
-            result['mdm_enrollment'] = result.get('mdm_enrollment', '')
+        if _p.returncode == 0:
+            if isinstance(_r, bytes):
+                _r = _r.decode('utf-8').strip()
+
+            for _l in _r.splitlines():
+                _l = _l.strip()
+
+                if 'DEP' in _l:
+                    result['enrolled_via_dep'] = _l.split(': ')[1].lower()
+
+                if 'MDM enrollment' in _l:
+                    result['mdm_enrollment'] = _l.split(': ')[1].lower()
+
+        return result
+
+    def _process(self):
+        """Process all conditions and generate the condition dictionary."""
+        result = dict()
+
+        result.update(self._enrolled_state())
 
         return result
 
