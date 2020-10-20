d144c42dc182aeaba7295280e4de97dfa36f1394
carlashley <carl@macapakr.com>
2020-10-20 19:35:33 +1000

    Refactor System Extensions

--- src/usr/local/munki/conditions/com.github.carlashley.munkicon.system-extensions.py
+++ src/usr/local/munki/conditions/com.github.carlashley.munkicon.system-extensions.py
@@ -8,6 +8,10 @@ except ImportError:
     from .munkicon import plist
     from .munkicon import worker
 
+# Keys: 'sys_ext_bundles'
+#       'sys_ext_teams'
+#       'sys_ext_team_bundle'
+
 
 class SystemExtensionPolicyConditions(object):
     """Whitelisted System Extension's as applied by MDM or set by user."""
@@ -15,13 +19,16 @@ class SystemExtensionPolicyConditions(object):
 
         self.conditions = self._process()
 
-    def _process(self):
-        """Process all conditions and generate the condition dictionary."""
-        result = {'sys_ext_teams': set(),
-                  'sys_ext_bundles': set(),
-                  'sys_ext_team_bundle': set()}
+    def _sys_exts(self):
+        """System Extensions."""
+        result = {'sys_ext_teams': list(),
+                  'sys_ext_bundles': list(),
+                  'sys_ext_team_bundle': list()}
 
         _db_file = '/Library/SystemExtensions/db.plist'
+        _sys_ext_teams = set()
+        _sys_ext_bundles = set()
+        _sys_ext_team_bundle = set()
 
         if os.path.exists(_db_file):
             _exts = plist.readPlist(path=_db_file)['extensions']
@@ -33,18 +40,27 @@ class SystemExtensionPolicyConditions(object):
                     _state = _e.get('state', None)
 
                     if _state == 'activated_enabled':
-                        if _bundle_id:
-                            result['sys_ext_bundles'].add(_bundle_id)
-
                         if _team_id:
-                            result['sys_ext_teams'].add(_team_id)
+                            _sys_ext_teams.add(_team_id)
+
+                        if _bundle_id:
+                            _sys_ext_bundles.add(_bundle_id)
 
                         if _bundle_id and _team_id:
-                            result['sys_ext_team_bundle'].add('{},{}'.format(_team_id, _bundle_id))
+                            _team_bundle_str = '{},{}'.format(_team_id, _bundle_id)
+                            _sys_ext_team_bundle.add(_team_bundle_str)
 
-                result['sys_ext_teams'] = list(result['sys_ext_teams'])
-                result['sys_ext_bundles'] = list(result['sys_ext_bundles'])
-                result['sys_ext_team_bundle'] = list(result['sys_ext_team_bundle'])
+                result['sys_ext_teams'] = list(_sys_ext_teams)
+                result['sys_ext_bundles'] = list(_sys_ext_bundles)
+                result['sys_ext_team_bundle'] = list(_sys_ext_team_bundle)
+
+        return result
+
+    def _process(self):
+        """Process all conditions and generate the condition dictionary."""
+        result = dict()
+
+        result.update(self._sys_exts())
 
         return result
 
