ddbd45d18f9c84042ccb2d47ec2e3dc3512dee82
Carl <carl@macapakr.com>
2020-10-20 14:44:08 +1000

    Fixed Python 2.7 exception when reading binary PLISTs

--- src/usr/local/munki/conditions/munkicon/plist.py
+++ src/usr/local/munki/conditions/munkicon/plist.py
@@ -1,8 +1,11 @@
 """Wrappers for plistlib"""
 import os
 import plistlib
+import shutil
+import subprocess
 
 from sys import version_info
+from xml.parsers.expat import ExpatError
 
 # plistlib.readPlist() and plistlib.writePlist() deprecated in Python 3.4+
 DEPRECATED = (version_info.major == 3 and version_info.minor > 4)
@@ -18,7 +21,19 @@ def readPlist(path):
             with open(path, 'rb') as _f:
                 result = plistlib.load(_f)
         else:
-            result = plistlib.readPlist(path)
+            try:
+                result = plistlib.readPlist(path)
+            except ExpatError:
+                _tmp_path = '/tmp/{}'.format(os.path.basename(path))
+                shutil.copy(path, _tmp_path)
+                _cmd = ['/usr/bin/plutil', '-convert', 'xml1', _tmp_path]
+
+                _p = subprocess.Popen(_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                _r, _e = _p.communicate()
+
+                if _p.returncode == 0:
+                    result = plistlib.readPlist(_tmp_path)
+                    os.remove(_tmp_path)
 
     return result
 
